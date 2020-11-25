f97054fd1ec6efa7b6f610ae07d21d7074607527
Allen Wu <72174493+allenwuny@users.noreply.github.com>
2020-11-25 16:58:59 -0500

    Update particle.R
    Updated code to match new project requirements

--- code/particle.R
+++ code/particle.R
@@ -1,11 +1,16 @@
+rm(list = ls())    #delete objects
+cat("\014")        #clear console
+
 library(readr)
 library(glmnet)
 library(tidyverse)
 library(randomForest)
+library(DescTools)
 
-#data <- read_table2("C:/Users/Allen/Downloads/MiniBooNE.txt", col_names = FALSE, skip = 1)
+data <- read_table2("C:/Users/Allen/Downloads/MiniBooNE.txt", col_names = FALSE, skip = 1)
 #data <- read_table2("https://archive.ics.uci.edu/ml/machine-learning-databases/00199/MiniBooNE_PID.txt", col_names = FALSE, skip = 1)
 
+start_time = proc.time()
 
 #These numbers can be found in the data file in the first line. They were excluded from the read_table2 line
 elec_num = 36499
@@ -14,7 +19,7 @@ n = elec_num + muon_num
 
 
 #Identify bad data/outliers
-bad_data = which(abs(data$X20) >= 999)
+bad_data = which(abs(data$X20) == 999)
 bad_elec = table(bad_data <= elec_num)[2]
 bad_muon = table(bad_data <= elec_num)[1]
 
@@ -24,94 +29,53 @@ data = data[-bad_data,]
 n = n - bad_elec - bad_muon
 train_size = round(n*0.9,0)
 test_size = n-train_size
-
+p = dim(data)[2]
 
 #Generate the response vector assigning 1 to electron neutrino signals and 0 to muon neutrino signals
 label = c(replicate(elec_num - bad_elec, 1), replicate(muon_num - bad_muon, 0))
 
+#Create an empty dataframe to store AUC values
+auc_table = data.frame(matrix(ncol = 4, nrow = 0))
+colnames(auc_table) = c("Round", "Model", "Dataset", "AUC")
 
-#Create empty dataframe to store error rates from each model
-error = data.frame(pass=numeric(), model=character(), type=character(), rate=numeric())
-
-
-#Function to return the threshold that yields the smallest difference between type I and II error from the training dataset
-get_thrs = function(prob, y) {
-  thrs_vector = c()
+get_auc = function (fit) { #function to create a dataframe of FPR and TPR for both train and test
   
+  beta0.hat = fit$a0
+  beta.hat = as.vector(fit$beta)
+  prob.train = exp(x_train %*% beta.hat + beta0.hat)/(1 + exp(x_train %*% beta.hat + beta0.hat))
+  prob.test = exp(x_test %*% beta.hat + beta0.hat)/(1 + exp(x_test %*% beta.hat + beta0.hat))
+  
+  roc = data.frame(matrix(ncol = 4, nrow = 0))
   for (i in seq(0, 1, by = 0.01)){
+    roc.y.hat.train             =        ifelse(prob.train > i, 1, 0)
+    roc.FP.train                =        sum(y_train[roc.y.hat.train==1] == 0) 
+    roc.TP.train                =        sum(roc.y.hat.train[y_train==1] == 1) 
+    roc.P.train                 =        sum(y_train==1) 
+    roc.N.train                 =        sum(y_train==0) 
+    roc.FPR.train               =        roc.FP.train/roc.N.train # false positive rate = type 1 error 
+    roc.TPR.train               =        roc.TP.train/roc.P.train # true positive rate = 1 - type 2 error
     
-    y_hat = ifelse(prob > i, 1, 0) #table(y.hat.train, y.train)
-    FP = sum(y[y_hat==1] == 0) # false positives = negatives in the data that were predicted as positive
-    TP = sum(y_hat[y==1] == 1) # true positives = positives in the data that were predicted as positive
-    P = sum(y==1) # total positives in the data
-    N = sum(y==0) # total negatives in the data
-    FPR = FP/N # false positive rate = type 1 error = 1 - specificity
-    TPR = TP/P # true positive rate = 1 - type 2 error = sensitivity
+    roc.y.hat.test              =        ifelse(prob.test > i,1,0) 
+    roc.FP.test                 =        sum(y_test[roc.y.hat.test==1] == 0) 
+    roc.TP.test                 =        sum(roc.y.hat.test[y_test==1] == 1) 
+    roc.P.test                  =        sum(y_test==1)
+    roc.N.test                  =        sum(y_test==0) 
+    roc.TN.test                 =        sum(roc.y.hat.test[y_test==0] == 0)
+    roc.FPR.test                =        roc.FP.test/roc.N.test # false positive rate = type 1 error 
+    roc.TPR.test                =        roc.TP.test/roc.P.test # true positive rate = 1 - type 2 error
     
-    type1 = FPR
-    type2 = 1 - TPR
-    
-    thrs_vector = c(thrs_vector, abs(type1 - type2))
+    roc = rbind(roc,c(roc.FPR.train, roc.TPR.train, roc.FPR.test, roc.TPR.test))
   }
-  
-  return((which.min(thrs_vector) - 1)/100)
-}
-
-
-#Functions to calculate the train and test error rates based on the model
-#Function returns overall error rate, type I error rate, type II error rate, and the result of the sigmoid function as a list in a vector
-train_error_rate = function(fit){
-  
-  beta0.hat = fit$a0
-  beta.hat = as.vector(fit$beta)
-  prob = exp(x_train %*% beta.hat + beta0.hat )/(1 + exp(x_train %*% beta.hat + beta0.hat))
-  
-  train_thrs = get_thrs(prob, y_train)
-  
-  y_hat = ifelse(prob > train_thrs, 1, 0) #table(y.hat.train, y.train)
-  FP = sum(y_train[y_hat==1] == 0) # false positives = negatives in the data that were predicted as positive
-  TP = sum(y_hat[y_train==1] == 1) # true positives = positives in the data that were predicted as positive
-  P = sum(y_train==1) # total positives in the data
-  N = sum(y_train==0) # total negatives in the data
-  FPR = FP/N # false positive rate = type 1 error = 1 - specificity
-  TPR = TP/P # true positive rate = 1 - type 2 error = sensitivity
-  
-  typeI_err = FPR
-  typeII_err = 1 - TPR
-  overall_err = mean(y_train != y_hat)
-  return(overall_err)
-  #return(c(overall_err, typeI_err, typeII_err, list(prob)))
-}
-
-
-test_error_rate = function(fit){
-  
-  beta0.hat = fit$a0
-  beta.hat = as.vector(fit$beta)
-  prob = exp(x_test %*% beta.hat + beta0.hat )/(1 + exp(x_test %*% beta.hat + beta0.hat))
-
-  test_thrs = get_thrs(prob, y_test)
-
-  y_hat = ifelse(prob > test_thrs, 1, 0) #table(y.hat.train, y.train)
-  FP = sum(y_test[y_hat==1] == 0) # false positives = negatives in the data that were predicted as positive
-  TP = sum(y_hat[y_test==1] == 1) # true positives = positives in the data that were predicted as positive
-  P = sum(y_test==1) # total positives in the data
-  N = sum(y_test==0) # total negatives in the data
-  FPR = FP/N # false positive rate = type 1 error = 1 - specificity
-  TPR = TP/P # true positive rate = 1 - type 2 error = sensitivity
-  
-  typeI_err = FPR
-  typeII_err = 1 - TPR
-  overall_err = mean(y_test != y_hat)
-  
-  return(overall_err)
-  #return(c(overall_err, typeI_err, typeII_err, list(prob)))
+  colnames(roc) = c("FPR_train", "TPR_train", "FPR_test", "TPR_test")
+  auc_train = AUC(x=roc$FPR_train, y=roc$TPR_train)
+  auc_test = AUC(x=roc$FPR_test, y=roc$TPR_test)
+  return(c(auc_train, auc_test))
 }
 
 total_start = proc.time()
 
-#Main for loop to run all the regression models
-for (i in c(1:1)){
+#Main for loop to run all the regression models using 0.9 train / 0.1 test
+for (i in c(1:50)){
 
   #Shuffle the predictors and labels
   set.seed(i)
@@ -128,76 +92,140 @@ for (i in c(1:1)){
   
   
   #Create a weight vector based on the number of electron neutrino signals in the training dataframe
-  weight = sum(y_train)/length(y_train)
-  w = sapply(y_train, function(i){if (i==1) 1 else weight})
+  # weight = sum(y_train)/length(y_train)
+  # w = sapply(y_train, function(i){if (i==1) 1 else weight})
   
   
   #Run cross validation on the training dataset to find optimal lambda for elastic net
-  cv_elnet_start = proc.time()
-  cv_elnet_fit = cv.glmnet(x_train, y_train, family="binomial", type.measure="class")
-  cv_elnet_time = proc.time() - cv_elnet_start
-  elnet_lambda = cv_elnet_fit$lambda[which.min(cv_elnet_fit$lambda)]
+  cv_elnet_fit = cv.glmnet(x_train, y_train, family="binomial", 
+                           alpha=0.5, type.measure="auc")
   
-  #Use the minimum lambda from cross validation to run elastic net logistic regression
-  elnet_start = proc.time()
-  elnet_fit = glmnet(x_train, y_train, family = "binomial", lambda = elnet_lambda, 
-                     weights = w, alpha = 0.5, type.measure="class", standardize = FALSE)
-  elnet_time = proc.time() - elnet_start
+  #Use the lambda with minimum error from CV to run elastic net logistic regression
+  elnet_fit = glmnet(x_train, y_train, family = "binomial", 
+                     lambda = cv_elnet_fit$lambda.min, alpha = 0.5)
+
+  auc_table = rbind(auc_table, c(i, "Elastic Net", 
+                                 "Train", get_auc(elnet_fit)[1])) 
+  auc_table = rbind(auc_table, c(i, "Elastic Net", 
+                                 "Test", get_auc(elnet_fit)[2]))
   
   #Run cross validation on the training dataset to find optimal lambda for lasso
-  cv_lasso_start = proc.time()
-  cv_lasso_fit = cv.glmnet(x_train, y_train, family="binomial", type.measure="class",
-                           weights = w, alpha = 1, standardize = FALSE)
-  cv_lasso_time = proc.time() - cv_lasso_start
-  lasso_lambda = cv_lasso_fit$lambda[which.min(cv_lasso_fit$lambda)]
+  cv_lasso_fit = cv.glmnet(x_train, y_train, family="binomial", 
+                           alpha = 1,  type.measure="auc")
   
-  #Use the minimum lambda from cross validation to run lasso logistic regression
-  lasso_start = proc.time()
-  lasso_fit = glmnet(x_train, y_train, family = "binomial", lambda = lasso_lambda,
-                     weights = w, alpha = 1, type.measure="class", standardize = FALSE)
-  lasso_time = proc.time() - lasso_start
+  #Use the lambda with minimum from CV to run lasso logistic regression
+  lasso_fit = glmnet(x_train, y_train, family = "binomial", 
+                     lambda = cv_lasso_fit$lambda.min, alpha = 1)
+
+  auc_table = rbind(auc_table, c(i, "Lasso", 
+                                 "Train", get_auc(lasso_fit)[1])) 
+  auc_table = rbind(auc_table, c(i, "Lasso", 
+                                 "Test", get_auc(lasso_fit)[2]))
   
   #Run cross validation on the training dataset to find optimal lambda for ridge
-  cv_ridge_start = proc.time()
-  cv_ridge_fit = cv.glmnet(x_train, y_train, family="binomial", type.measure="class",
-                           weights = w, alpha = 0, standardize = FALSE)
-  cv_ridge_time = proc.time() - cv_ridge_start
-  ridge_lambda = cv_ridge_fit$lambda[which.min(cv_ridge_fit$lambda)]
+  cv_ridge_fit = cv.glmnet(x_train, y_train, family="binomial", 
+                           alpha = 0, type.measure="auc")
   
-  #Use the minimum lambda from cross validation to run ridge logistic regression
-  ridge_start = proc.time()
-  ridge_fit = glmnet(x_train, y_train, family = "binomial", lambda = ridge_lambda,
-                     weights = w, alpha = 0, type.measure="class", standardize = FALSE)
-  ridge_time = proc.time() - ridge_start
+  #Use the lambda with minimum error from CV to run ridge logistic regression
+  ridge_fit = glmnet(x_train, y_train, family = "binomial", 
+                     lambda = cv_ridge_fit$lambda.min, alpha = 0)
+
+  auc_table = rbind(auc_table, c(i, "Ridge", 
+                                 "Train", get_auc(ridge_fit)[1])) 
+  auc_table = rbind(auc_table, c(i, "Ridge", 
+                                 "Test", get_auc(ridge_fit)[2]))
   
   
-  #Append error rates of each model to the error df
-  error = rbind(error, c(i, "Elastic Net","Train", train_error_rate(elnet_fit)))
-  error = rbind(error, c(i, "Elastic Net", "Test", test_error_rate(elnet_fit)))
-  error = rbind(error, c(i, "Lasso", "Train", train_error_rate(lasso_fit)))
-  error = rbind(error, c(i, "Lasso", "Test", test_error_rate(lasso_fit)))
-  error = rbind(error, c(i, "Ridge", "Train", train_error_rate(ridge_fit)))
-  error = rbind(error, c(i, "Ridge", "Test", test_error_rate(ridge_fit)))
+  dat = data.frame(x=x_train, y = as.factor(y_train))
+  rf.fit     =    randomForest(y~., data = dat, mtry = sqrt(p))
+  y.hat      =    predict(rf.fit, dat)
   
+  print(table(y.hat,y_train))
+  print(mean(y.hat==y_train))
+  print(mean(y.hat[y_train=="1"]==y_train[y_train=="1"]))
+  print( mean(y.hat[y_train=="0"]==y_train[y_train=="0"]))
   
   #Gives an indication of how far the loop has progressed
-  cat("Pass", i, "complete. ")
+  cat("Round", i, "complete. ")
 }
 
-total_time = proc.time() - total_start
 
 #Fix the column names for the error df
-colnames(error) = c("pass", "model", "type", "rate")
+# colnames(error) = c("pass", "model", "type", "rate")
+colnames(auc_table) =  c("Round", "Model", "Dataset", "AUC")
 
+
+data = data.matrix(data)
+label = as.numeric(label)
+
+
+#Run cross validation on the entire dataset to find optimal lambda for elastic net
+cv_elnet_start = proc.time()
+cv_elnet_fit = cv.glmnet(data, label, family="binomial", 
+                         alpha=0.5, type.measure="auc")
+
+#Use the minimum lambda from cross validation to run elastic net logistic regression
+elnet_fit = glmnet(data, label, family = "binomial", 
+                   lambda = cv_elnet_fit$lambda.min, alpha = 0.5)
+elnet_time = proc.time() - cv_elnet_start
+
+
+#Run cross validation on the entire dataset to find optimal lambda for lasso
+cv_lasso_start = proc.time()
+cv_lasso_fit = cv.glmnet(data, label, family="binomial", 
+                         alpha = 1,  type.measure="auc")
+
+#Use the minimum lambda from cross validation to run lasso logistic regression
+lasso_fit = glmnet(data, label, family = "binomial", 
+                   lambda = cv_lasso_fit$lambda.min, alpha = 1)
+lasso_time = proc.time() - cv_lasso_start
+
+
+#Run cross validation on the entire dataset to find optimal lambda for ridge
+cv_ridge_start = proc.time()
+cv_ridge_fit = cv.glmnet(data, label, family="binomial", 
+                         alpha = 0, type.measure="auc")
+
+#Use the minimum lambda from cross validation to run ridge logistic regression
+ridge_fit = glmnet(data, label, family = "binomial", 
+                   lambda = cv_ridge_fit$lambda.min, alpha = 0)
+ridge_time = proc.time() - cv_ridge_start
 
 #Generate boxplot of error rates
-ggplot(error, aes(x=model, y=as.numeric(rate), fill = type)) + geom_boxplot()
+# ggplot(error, aes(x=model, y=as.numeric(rate), fill = type)) + geom_boxplot()
+ggplot(auc_table) + 
+  aes(x=Dataset, y=as.numeric(AUC)) + 
+  geom_boxplot()
+
+#Generate barplot of beta coefficients
+el_beta = cbind(as.vector(elnet_fit$beta),rep("Elastic Net",50), seq(1,50,by=1))
+ls_beta = cbind(as.vector(lasso_fit$beta),rep("Lasso",50), seq(1,50,by=1))
+rd_beta = cbind(as.vector(ridge_fit$beta),rep("Ridge",50), seq(1,50,by=1))
+
+el_beta = data.frame(el_beta)
+ls_beta = data.frame(ls_beta)
+rd_beta = data.frame(rd_beta)
+
+colnames(el_beta) = c("beta", "model", "coef")
+colnames(ls_beta) = c("beta", "model", "coef")
+colnames(rd_beta) = c("beta", "model", "coef")
+
+el_order = el_beta %>% arrange(as.numeric(beta)) %>% select(coef)
+el_order = as.numeric(el_order[["coef"]])
+
+el_beta = el_beta[el_order,]
+ls_beta = ls_beta[el_order,]
+rd_beta = rd_beta[el_order,]
+
+beta_df = rbind(el_beta, ls_beta, rd_beta)
+
+beta_df = beta_df %>% mutate(pos = rep(seq(1,50,by=1),3))
 
 
-#Generate histogram of beta coefficients
-beta = rbind(cbind(as.vector(elnet_fit$beta),rep("Elastic",50)), 
-             cbind(as.vector(lasso_fit$beta),rep("Lasso",50)), 
-             cbind(as.vector(ridge_fit$beta),rep("Ridge",50)))
-beta = data.frame(beta)
-colnames(beta) = c("betas", "model")
-ggplot(beta) + aes(x=as.numeric(betas)) + geom_histogram(bins = 50) + facet_grid(model ~ .)
+total_time = proc.time() - start_time
+
+ggplot(beta_df) + 
+  aes(x=as.numeric(pos), y = as.numeric(beta)) + 
+  geom_col() + 
+  facet_grid(model ~ .)
+
